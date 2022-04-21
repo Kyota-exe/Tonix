a8c9f3c95ec17b3c6c390b28d54c68eee6af79bf
Kyota Poeti <kyota.exe@gmail.com>
2022-04-21 18:55:32 -0400

    Implement stub for time (sys_clock_get) syscall
    Since it is technically within the spec to return 0 when sys_clock_get
    is called, implement this functioanlity as a placeholder.
    
    Add descriptive printing for each system call, which replaces the
    "syscall {SYSCALL_ID}" printing in SystemCall.cpp.

--- kernel/src/SystemCall.cpp
+++ kernel/src/SystemCall.cpp
@@ -7,13 +7,6 @@
 uint64_t SystemCall(SystemCallType type, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3,
                     uint64_t arg4, uint64_t arg5, InterruptFrame* interruptFrame, Error& error)
 {
-    // PLACEHOLDER
-
-    if (type != SystemCallType::Log)
-    {
-        Serial::Printf("syscall %d", type);
-    }
-
     Scheduler* scheduler = Scheduler::GetScheduler();
 
     switch (type)
