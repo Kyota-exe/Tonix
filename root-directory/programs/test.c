b0288a6b4abfe4506d5acc6dfb61227ab20a0045
Kyota Poeti <kyota.exe@gmail.com>
2022-05-07 00:05:18 -0400

    Update test program

--- root-directory/programs/test.c
+++ root-directory/programs/test.c
@@ -3,9 +3,13 @@
 
 int main()
 {
-	int some = 5;
-	printf("Hello world: %d\n", some);
-	some += 4;
-	printf("Updated value: %d", some);
-	return 0;
+    int some = 5;
+    printf("Hello world: %d\n", some);
+    some += 4;
+    sleep(1);
+    printf("Enter a number: ");
+    fflush(stdout);
+    scanf("%d", &some);
+    printf("You entered %d\n", some);
+    return 0;
 }
