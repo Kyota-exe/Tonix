62cc50991bf3594029a8876c45ba795669327418
Kyota Poeti <kyota.exe@gmail.com>
2022-05-06 20:27:54 -0400

    Refactor stack initialization in ELF::LoadELF

--- kernel/src/ELF.cpp
+++ kernel/src/ELF.cpp
@@ -73,49 +73,58 @@ void ELF::LoadELF(const String& path, PagingManager* pagingManager, uintptr_t& e
     }
     else if (elfHeader->type == ELFType::Executable)
     {
+        Assert(USER_STACK_SIZE % 0x1000 == 0);
         uint64_t stackPageCount = USER_STACK_SIZE / 0x1000;
         uintptr_t stackLowestVirtAddr = USER_STACK_BASE - USER_STACK_SIZE;
-        uintptr_t stackLowestPhysAddr = RequestPageFrames(stackPageCount);
-        void* stackPhysAddr = nullptr;
-        for (uint64_t page = 0; page < USER_STACK_SIZE; page += 0x1000)
+
+        uintptr_t stackPhysAddr {};
+        for (uint64_t pageIndex = 0; pageIndex < stackPageCount; ++pageIndex)
         {
-            stackPhysAddr = reinterpret_cast<void*>(stackLowestPhysAddr + page);
-            auto virtAddr = reinterpret_cast<void*>(stackLowestVirtAddr + page);
-            pagingManager->MapMemory(virtAddr, stackPhysAddr, true);
+            auto virtAddr = reinterpret_cast<void*>(stackLowestVirtAddr + pageIndex * 0x1000);
+            stackPhysAddr = RequestPageFrame();
+            pagingManager->MapMemory(virtAddr, reinterpret_cast<void*>(stackPhysAddr), true);
         }
 
-        uintptr_t stackHigherHalfAddr = HigherHalf(reinterpret_cast<uintptr_t>(stackPhysAddr)) + 0x1000;
+        uintptr_t stackHigherHalfAddr = HigherHalf(stackPhysAddr + 0x1000);
         auto stackHigherHalf = reinterpret_cast<uintptr_t*>(stackHigherHalfAddr);
 
         if (hasDynamicLinking)
         {
+            auto push = [stackHigherHalfAddr, &stackHigherHalf] (uint64_t value)
+            {
+                uintptr_t usedStackSize = stackHigherHalfAddr - reinterpret_cast<uintptr_t>(stackHigherHalf);
+                Assert(usedStackSize + sizeof(value) <= 0x1000);
+                *--stackHigherHalf = value;
+            };
+
             // Auxiliary vector
-            *--stackHigherHalf = 0; // NULL
-            *--stackHigherHalf = 0;
-            *--stackHigherHalf = programHeaderTableAddr;
-            *--stackHigherHalf = 3;
-            *--stackHigherHalf = elfHeader->programHeaderTableEntrySize;
-            *--stackHigherHalf = 4;
-            *--stackHigherHalf = elfHeader->programHeaderTableEntryCount;
-            *--stackHigherHalf = 5;
-            *--stackHigherHalf = elfHeader->entry;
-            *--stackHigherHalf = 9;
+            push(0); // NULL
+            push(0);
+            push(programHeaderTableAddr);
+            push(3);
+            push(elfHeader->programHeaderTableEntrySize);
+            push(4);
+            push(elfHeader->programHeaderTableEntryCount);
+            push(5);
+            push(elfHeader->entry);
+            push(9);
 
             // Environment
-            *--stackHigherHalf = 0;
+            push(0); // NULL
 
             // Argument vector (argv)
-            *--stackHigherHalf = 0; // NULL
+            push(0); // NULL
 
             // Argument count (argc)
-            *--stackHigherHalf = 0;
+            push(0);
         }
         else
         {
             entry = elfHeader->entry;
         }
 
-        stackPtr = USER_STACK_BASE - (stackHigherHalfAddr - reinterpret_cast<uintptr_t>(stackHigherHalf));
+        uintptr_t usedStackSize = stackHigherHalfAddr - reinterpret_cast<uintptr_t>(stackHigherHalf);
+        stackPtr = USER_STACK_BASE - usedStackSize;
     }
 
     delete elfHeader;
