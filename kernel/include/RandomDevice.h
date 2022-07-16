6917f7a4eb4ddce895fdc0ace117d6cc1bdee34e
Kyota Poeti <kyota.exe@gmail.com>
2022-07-16 15:07:53 -0400

    Implement basic generator for RandomDevice

--- kernel/include/RandomDevice.h
+++ kernel/include/RandomDevice.h
@@ -8,4 +8,6 @@ public:
     uint64_t Read(void* buffer, uint64_t count, uint64_t position) override;
     uint64_t Write(const void* buffer, uint64_t count, uint64_t position) override;
     RandomDevice(const String& name, uint32_t inodeNum);
+private:
+    uint64_t seed = 5323;
 };
--- kernel/src/RandomDevice.cpp
+++ kernel/src/RandomDevice.cpp
@@ -2,10 +2,16 @@
 
 uint64_t RandomDevice::Read(void* buffer, uint64_t count, uint64_t position)
 {
-    Panic();
-    (void)buffer;
-    (void)count;
-    (void)position;
+    Assert(position == 0);
+
+    auto data = static_cast<uint8_t*>(buffer);
+    for (uint64_t i = 0; i < count; ++i)
+    {
+        seed = (8253729 * seed + 2396403);
+        data[i] = (seed % 32767) & 0xff;
+    }
+
+    return count;
 }
 
 uint64_t RandomDevice::Write(const void* buffer, uint64_t count, uint64_t position)
